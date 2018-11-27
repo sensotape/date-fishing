9c101677a6e37cef95a73a0b8627b0cf6ae189f4
Aigerim Kalysheva <sensotape@gmail.com>
2018-11-27 17:02:47 +0100

    fonts and color

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_27_113751) do
+ActiveRecord::Schema.define(version: 2018_11_27_151606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2018_11_27_113751) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.json "photo"
     t.index ["user_id"], name: "index_experiences_on_user_id"
   end
 
@@ -79,6 +80,7 @@ ActiveRecord::Schema.define(version: 2018_11_27_113751) do
     t.string "seeking", null: false
     t.text "bio"
     t.datetime "birthday", null: false
+    t.json "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
