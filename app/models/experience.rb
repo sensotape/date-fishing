97a78c5f59b3d5faf3c9d7e08fd2f1f267973083
Aigerim Kalysheva <sensotape@gmail.com>
2018-11-27 15:00:40 +0100

    geocoder for location of the experience added

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -8,8 +8,12 @@ class Experience < ApplicationRecord
   belongs_to :user
   has_many :nibbles
 
+  geocoded_by :location
+
   validates :title, presence: true, length: { in: 1..50 }
   validates :category, presence: true, inclusion: { in: CATEGORIES }
   validates :location, length: { maximum: 120 }
   validates :description, presence: true
+
+  after_validation :geocode, if: :will_save_change_to_address?
 end
