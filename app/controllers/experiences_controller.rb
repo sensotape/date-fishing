e6ca096884906e1b47eea8f7130f557eb36e2850
marcel fonseca <marcelsf23br@gmail.com>
2018-11-28 12:21:58 +0100

    added map and geocoding

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -1,6 +1,15 @@
 class ExperiencesController < ApplicationController
-
+  before_action :set_experience, only: [:show, :edit, :update, :destroy]
   def show
+    @markers = Experience.where(id: params[:id]).map do |flat|
+      {
+        lng: flat.longitude,
+        lat: flat.latitude
+      }
+    end
+  end
+
+  def set_experience
     @experience = Experience.find(params[:id])
     authorize @experience
   end
