f29925d17d138a9424cb8f623d9876c9d83b8ff8
marcel fonseca <marcelsf23br@gmail.com>
2018-11-28 15:54:02 +0100

    circle radius has been made

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,6 +1,6 @@
 import 'mapbox-gl/dist/mapbox-gl.css'
 import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js';
-
+const MapboxCircle = require('mapbox-gl-circle');
 const mapElement = document.getElementById('map');
 
 if (mapElement) { // only build a map if there's a div#map to inject into
@@ -11,12 +11,37 @@ if (mapElement) { // only build a map if there's a div#map to inject into
   });
 
   const markers = JSON.parse(mapElement.dataset.markers);
+    let editableOpts = {
+        editable: false,
+        strokeColor: '#29AB87',
+        strokeWeight: 1,
+        strokeOpacity: 0.85,
+        fillColor: 'red',
+        fillOpacity: 0.2,
+        minRadius: 100,
+        maxRadius: 500000,
+        // debugEl: document.body.appendChild(document.createElement('div'))
+    };
 
+    let nonEditableOpts = {
+        strokeWeight: 0,
+        fillColor: '#000000',
+        fillOpacity: 0.2
+    };
   markers.forEach((marker) => {
-    new mapboxgl.Marker()
-      .setLngLat([marker.lng, marker.lat])
-      .addTo(map);
-  })
+    // let extraPrettyEditableOpts = _.extend({refineStroke: true}, editableOpts);
+
+    // new mapboxgl.Marker()
+    //   .setLngLat([marker.lng, marker.lat])
+    //   .addTo(map);
+
+
+    // const extraPrettyEditableOpts = _.extend({refineStroke: true}, editableOpts);
+
+    window.editableCircle0 = new MapboxCircle({lat: marker.lat, lng: marker.lng}, 350, editableOpts).addTo(map);
+
+  });
+    // window.editableCircle0 = new MapboxCircle({lat: 39.986, lng: -75.341}, 350, editableOpts).addTo(map);
 
   if (markers.length === 0) {
     map.setZoom(1);
