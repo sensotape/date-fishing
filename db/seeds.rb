490fe48b99d69009d1d079547e72fb001778fb91
May-Lee Sia <mayleesia@gmail.com>
2018-11-30 16:49:48 +0100

    Add new seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,8 @@ andrew = User.create!(
   last_name: 'Erlanger',
   password: '123456',
   password_confirmation: '123456',
-  bio: "Looking for someone straight up from Down Under? Let's make a date.",
+  bio: "Super seriously totally straight guy from Down Under looking for new experiences.
+  I'll try anything once. No butt stuff.",
   gender: 'Male',
   seeking: 'Female',
   birthday: Date.new(1988, 2, 26)
@@ -22,10 +23,8 @@ marcel = User.create!(
   last_name: 'Fonseca',
   password: '123456',
   password_confirmation: '123456',
-  bio: "Bacon ipsum dolor amet venison andouille short loin leberkas alcatra rump shank tri-tip
-  buffalo. Chuck turkey kielbasa biltong. Cow biltong andouille, shank bacon ball tip tri-tip
-  turkey filet mignon buffalo strip steak porchetta kielbasa burgdoggen sirloin. Meatball bresaola
-  pig pork chop beef sirloin.",
+  bio: "Hi there, I'm new to Berlin from Brazil and looking to meet new people. I'm super chill, love going
+  to the movies and spending time with friends.",
   gender: 'Male',
   seeking: 'Female',
   birthday: Date.new(1987, 10, 30)
@@ -36,8 +35,7 @@ maylee = User.create!(
   last_name: 'Sia',
   password: '123456',
   password_confirmation: '123456',
-  bio: "Fresh out of my dirty thirties, ready for my jorty forties. You're only young once,
-  but you can be immature forever!",
+  bio: "Old enough to know better, young enough not to care :)",
   gender: 'Female',
   seeking: 'Male',
   birthday: Date.new(1978, 5, 18)
@@ -48,11 +46,8 @@ aigerim = User.create!(
   last_name: 'Kalysheva',
   password: '123456',
   password_confirmation: '123456',
-  bio: "Bacon Pancakes\n
-        Making Bacon Pancakes\n
-        Take some bacon and I'll put it in a pancake\n
-        Bacon Pancakes That's what its gonna make\n
-        Bacon Pancakes",
+  bio: "Till death do we part? Looking for my ride or die. Meet for a once-in-a-lifetime experience.
+  Tell your family you went to live in a Buddhist monastery.",
   gender: 'Female',
   seeking: 'Male',
   birthday: Date.new(1993, 3, 4)
@@ -69,78 +64,67 @@ tango_date = aigerim.experiences.new(
   category: 'Art & Culture',
   location: 'Gesundbrunnen, Berlin',
   description: "I learned to tango in Argentina and would like to share my skills in a romantic evening.",
-  start_date: Date.new(2018, 12, 12),
-  end_date: Date.new(2018, 12, 12)
+  date: Date.new(2018, 12, 12)
 )
 tango_date.save!
 tango_date.photos.create!(picture: open('https://res.cloudinary.com/dcjfhbk0f/image/upload/v1543318203/come_tango_dancing_at_ballroom_berlin.png'))
-matrix_date = marcel.experiences.new(
-  title: 'Hardcore Clubbing at Matrix',
-  category: 'Music',
-  location: 'Warschauerstr., Berlin',
-  description: "Wanna dance like crazy with someone SEZY at Matrix?",
-  start_date: Date.new(2018, 12, 7),
-  end_date: Date.new(2018, 12, 9)
-)
-matrix_date.save!
-matrix_date.photos.create!(picture: open('https://res.cloudinary.com/dcjfhbk0f/image/upload/v1543317952/party_like_crazy_at_MATRIX.png'))
+
 copenhagen_date = andrew.experiences.new(
   title: 'Bike Ride to Copenhagen',
   category: 'Tourism',
   location: 'Copenhagen, Denmark',
   description: "Hi, I'd love to go on a romantic trip to Copenhagen by bike",
-  start_date: Date.new(2018, 12, 18),
-  end_date: Date.new(2018, 12, 19)
+  date: Date.new(2018, 12, 18)
 )
 copenhagen_date.save!
 copenhagen_date.photos.create!(picture: open('https://res.cloudinary.com/dcjfhbk0f/image/upload/v1543318234/winter_trip_to_copenhagen.png'))
+
 boardgames_date = marcel.experiences.new(
   title: 'Board Games and Red Wine',
   category: 'Geek & Nerd',
   location: '77 Rigaerstr., Berlin',
   description: "A quiet night of boardgames and a bottle of Shafer One Point Five cabernet.",
-  start_date: Date.new(2018, 12, 17),
-  end_date: Date.new(2018, 12, 17)
+  date: Date.new(2018, 12, 17)
 )
 boardgames_date.save!
 boardgames_date.photos.create!(picture: open('https://res.cloudinary.com/dcjfhbk0f/image/upload/v1543318203/boardgames_and_red_wine.png'))
+
 skydiving_date = aigerim.experiences.new(
   title: 'Jump out of a Plane with Me',
   category: 'Bucketlist',
   location: 'Germany',
   description: "It's time to finally take the plunge! Let's go Skydiving this Weekend!",
-  start_date: Date.new(2018, 12, 20),
-  end_date: Date.new(2018, 12, 21)
+  date: Date.new(2018, 12, 20)
 )
 skydiving_date.save!
 skydiving_date.photos.create!(picture: open('https://res.cloudinary.com/dcjfhbk0f/image/upload/v1543318234/skydiving.png'))
+
 exploration_date = maylee.experiences.new(
   title: 'Explore the Abandoned Brewery',
   category: 'Adventure',
   location: 'Berlin, Germany',
   description: "We meet two hours before dusk. Bring your own gear. Experience required!",
-  start_date: Date.new(2018, 12, 26),
-  end_date: Date.new(2018, 12, 26)
+  date: Date.new(2018, 12, 26)
 )
 exploration_date.save!
 exploration_date.photos.create!(picture: open('https://res.cloudinary.com/dcjfhbk0f/image/upload/v1543318203/explore_the_abandoned_brewery_in_pankow.png'))
+
 sushi_date = maylee.experiences.new(
   title: 'Sushi and Sake',
   category: 'Food & Drink',
   location: 'Prenzlauerberg, Berlin',
   description: "Let the good times roll! The freshest sushi and sashimi, paired with amazing sakes.",
-  start_date: Date.new(2018, 12, 21),
-  end_date: Date.new(2018, 12, 21)
+  date: Date.new(2018, 12, 21)
 )
 sushi_date.save!
 sushi_date.photos.create!(picture: open('https://res.cloudinary.com/dcjfhbk0f/image/upload/v1543318203/cook_sushi_with_me.png'))
+
 badminton_date = andrew.experiences.new(
   title: 'Beat Me at Badminton',
   category: 'Sport & Recreation',
   location: 'Sportcenter Wittenau',
   description: "Up to see if we're a match? My record stands at undefeated, but maybe you can change that!",
-  start_date: Date.new(2018, 12, 27),
-  end_date: Date.new(2018, 12, 27)
+  date: Date.new(2018, 12, 27)
 )
 badminton_date.save!
 badminton_date.photos.create!(picture: open('https://res.cloudinary.com/dcjfhbk0f/image/upload/v1543318203/beat_me_at_badminton.png'))
